552bd90e8d80accb62bac328c60642b30f9d4b32
Alexander Golubev <alexander.golubev@gmail.com>
2015-02-12 16:27:38 -0500

    Add proper repo to build script

--- project/Build.scala
+++ project/Build.scala
@@ -8,16 +8,17 @@ object ApplicationBuild extends Build {
     new File("."),
     settings = BuildSettings.buildSettings ++ Seq(
       libraryDependencies := Dependencies.runtime,
-      publishMavenStyle := true
+      publishMavenStyle := true,
+      resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
     )
   )
 
   object Dependencies {
     val runtime = Seq(
-      "com.typesafe.play" %% "play" % "2.2.2" % "provided" notTransitive(),
+      "com.typesafe.play" %% "play" % "2.2.2",
       "com.typesafe" % "config" % "1.0.2",
-      "ch.qos.logback" % "logback-core" % "1.0.13" % "provided",
-      "ch.qos.logback" % "logback-classic" % "1.0.13" % "provided",
+      "ch.qos.logback" % "logback-core" % "1.0.13",
+      "ch.qos.logback" % "logback-classic" % "1.0.13",
       "org.scala-lang" % "scala-library" % BuildSettings.buildScalaVersion,
       "org.apache.zookeeper" % "zookeeper" % "3.4.6" notTransitive() exclude("org.slf4j", "slf4j-log4j12"),
       "org.scalatest" %% "scalatest" % "1.9.1" % "test",
@@ -27,7 +28,7 @@ object ApplicationBuild extends Build {
 
   object BuildSettings {
     val buildOrganization = "com.github.agolubev"
-    val buildVersion = "1.3"
+    val buildVersion = "0.3"
     val buildScalaVersion = "2.10.3"
     val buildSbtVersion = "0.13"
     val buildSettings = Defaults.defaultSettings ++ Seq(
