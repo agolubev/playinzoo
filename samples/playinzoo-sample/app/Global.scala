0bc11e99d78c7ca416a4381fbc37526ea98a6b7f
Alexander Golubev <alexander.golubev@gmail.com>
2015-02-13 00:31:40 -0500

    Add sample application

---
+++ samples/playinzoo-sample/app/Global.scala
@@ -0,0 +1,23 @@
+import java.io.File
+
+import com.typesafe.config.ConfigFactory
+import play.api.Mode.Mode
+import play.api._
+
+import scala.collection.immutable
+
+object Global extends GlobalSettings {
+
+  override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode): Configuration = {
+    config ++ Configuration.from(immutable.Map("key"->"value"))
+  }
+
+  override def onStart(app: Application) {
+    Logger.info("Application has started")
+  }
+
+  override def onStop(app: Application) {
+    Logger.info("Application shutdown...")
+  }
+
+}
